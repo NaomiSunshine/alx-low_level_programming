8542a30a0faf60a9e783e3dda2cb5cc5520bc8d0
NaomiSunshine <Naomikenneth33@gmail.com>
2023-09-14 18:23:08 -0700

    sqaure

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_square - prints a square, followed by a new line;
  * @size: size of the square
@@ -11,6 +12,7 @@ void print_square(int size)
 	} else
 	{
 		int i, j;
+
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
